11b7d0134625080d4834aaef5ee29cb8e1e75e6c
Suraksha <surakshak571@gmail.com>
2022-08-18 21:15:41 +0500

    adminboard responsive

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,18 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(MaterialApp(
+    builder: (context, child) => ResponsiveWrapper.builder(child, maxWidth: 1200, 
+      minWidth: 480, defaultScale: true,
+      breakpoints: [ResponsiveBreakpoint.resize(480, name: MOBILE),
+      ResponsiveBreakpoint.autoScale(800, name: TABLET),
+      ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+      ],
+      background: Container(color: Color(0xFFF5F5F5))),
+      initialRoute: "/",
     debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
